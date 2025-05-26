7a086627fd90a37539bf78f65f9054249d39faa6
Renzo Santos <orrego.st.santos@gmail.com>
2025-05-26 17:37:57 -0500

    delete hook Backgroundaudio

--- src/hooks/useBackgroundaudio.js
+++
@@ -1,9 +0,0 @@
-import { BakcgroundAudioContext } from 'providers/BackgroundAudioProvider';
-import { useContext } from 'react';
-
-const UseBackgroundaudio = () => {
-  const { localUrl, isReady, setIndex } = useContext(BakcgroundAudioContext);
-  return <div></div>;
-};
-
-export default UseBackgroundaudio;
